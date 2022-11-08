1f5ea4558c4ea0ea71faf5018bab92033185f2a7
SophIren <45230353+SophIren@users.noreply.github.com>
2022-11-09 00:37:31 +0300

    Initial parser structure (#1)

---
+++ fat_parser/boot_sector/__init__.py
@@ -0,0 +1,1 @@
+from boot_sector_parser import BootSectorParser
---
+++ fat_parser/raw_parser.py
@@ -0,0 +1,7 @@
+from boot_sector import BootSectorParser
+from io_manager import IOManager
+
+
+class RawFatParser:
+    def __init__(self, io_manager: IOManager):
+        pass
